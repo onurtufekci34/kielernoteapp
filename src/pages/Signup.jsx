ed1406bc50e491b8222930c790c00dd71c2d5a83
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-23 16:48:15 +0100

    sign up

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,49 +1,65 @@
-import {useState} from 'react'
-
-
-
-
+import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 export default function Signup() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const [userName, setUserName] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [confirmPassword,setConfirmPassword] = useState("")
-    
-    const hadnleSubmit = async (e) => {
-        e.preventDefault()
+  const { signup, isPending, error } = useSignup();
 
-        console.log(userName,email,password,confirmPassword)
-        setUserName("")
-        setEmail("")
-        setPassword("")
-        setConfirmPassword("")
-        
+  const hadnleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password === confirmPassword) {
+      signup(email, password, userName);
+    } else {
+      alert("passwords did not match");
     }
 
-
+    // console.log(userName,email,password,confirmPassword)
+    // setUserName("")
+    // setEmail("")
+    // setPassword("")
+    // setConfirmPassword("")
+  };
 
   return (
-    <form className='signup' onSubmit={hadnleSubmit}>
-        <h3>Kieler Note App Signup Page</h3>
+    <form className="signup" onSubmit={hadnleSubmit}>
+      <h3>Kieler Note App Signup Page</h3>
 
-        <label>Your Name:</label>
-        <input type="text" onChange={(e)=> setUserName(e.target.value)} value={userName} />
+      <label>Your Name:</label>
+      <input
+        type="text"
+        onChange={(e) => setUserName(e.target.value)}
+        value={userName}
+      />
 
-        <label>Your E-Mail:</label>
-        <input type="email" onChange={(e)=> setEmail(e.target.value)} value={email} />
+      <label>Your E-Mail:</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
 
-        <label>Your Password:</label>
-        <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
-        
-        <label>Confirm Your Password:</label>
-        <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}/>
-        
+      <label>Your Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
 
-        <button>Signup</button>
+      <label>Confirm Your Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        value={confirmPassword}
+      />
 
-        
+      {!isPending && <button>Signup</button>}
+      {isPending && <button disabled>Your process is ongoing</button>}
+      {error && <div className="error">{error}</div>}
     </form>
-  )
+  );
 }
