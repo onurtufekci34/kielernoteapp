7b08bd1d228be39d888e77fa45dcbdf94c4254ab
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-29 15:34:46 +0100

    authContext added

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext,useReducer } from "react";
+
+export const AuthContext=createContext();
+
+export const authReducer=(state,action)=>{
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
+
+export const AuthContextProvider=({children})=>{
+
+    const [state,dispatch]=useReducer(authReducer,{
+        user:null
+    })
+	
+	console.log('Auth Context: ',state);
+
+    return (
+        <AuthContext.Provider value={{...state,dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
