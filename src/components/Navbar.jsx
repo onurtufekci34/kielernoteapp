1a024afbc01da67976f094ff16e26e2c3715f1b0
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-30 10:15:22 +0100

    logout process created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
-   <header>
-    <div className="container">
+    <header>
+      <div className="container">
         <Link to="/">
-        <h1>Kieler Notebook</h1>
+          <h1>Kieler Notebook</h1>
         </Link>
         <nav>
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
-
+          </div>
+          <div>
+            <button onClick={logout}>Logout</button>
+            
           </div>
         </nav>
-    </div>
-   </header>
-  )
+      </div>
+    </header>
+  );
 }
