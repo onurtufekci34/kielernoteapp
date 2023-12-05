d5b5f89c008ec1fc01377668aadab38ecd21aadd
onurtufekci34 <onurtufekci61@gmail.com>
2023-12-05 22:12:28 +0100

    some updates

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,15 +2,17 @@ import Notfoundgif from '../assets/notfound.webp'
 
 export default function NotFound() {
   return (
-    <div className="container">
-    <div className="row">
-      <div className="col-md-6 offset-md-3 text-center mt-5">
-        <h1 className="display-4">404</h1>
-        <p className="lead">Oops! Page not found.</p>
-        <p>Sorry, the page you are looking for might have been removed or doesn't exist.</p>
-        <img className='w-25' src={Notfoundgif} alt="not found gif" />
+    <div className="notfound">
+      <div className="notfound_container">
+        <div>
+          <h1 className="title">404</h1>
+          <p className="description">Oops! Page not found.</p>
+          <p className="info">
+            Sorry, the page you are looking for might have been removed or doesn't exist.
+          </p>
+          <img className="not-found-img" src={Notfoundgif} alt="not found gif" />
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
