ccde74f1a0671063a5787146a803a91ad6e88a79
onurtufekci34 <onurtufekci61@gmail.com>
2023-12-05 18:59:57 +0100

    some updates

--- src/components/NoteDetaild.jsx
+++ src/components/NoteDetaild.jsx
@@ -1,20 +1,21 @@
-import {useFirestore} from '../hooks/useFirestore'
-import moment from 'moment'
-import 'moment/locale/de' 
-
-
+import { useFirestore } from '../hooks/useFirestore';
+import moment from 'moment';
+import 'moment/locale/de';
 
 export default function NoteDetaild({ not }) {
+  const { deleteDocument } = useFirestore("nots");
 
-  const {deleteDocument} = useFirestore("nots")
-
+  let formattedDate = "";
+  if (not.date && not.date.toDate) {
+    formattedDate = moment(not.date.toDate()).format('L');
+  }
 
   return (
     <div className="not-detay">
       <h4>{not.title}</h4>
       <p>{not.explanation}</p>
-      <p className="zaman">{moment(new Date(not.date.toDate())).format('L')}</p>
-      <span onClick={()=>deleteDocument(not.id)} className='material-symbols-outlined'>delete_forever</span>
+      <p className="zaman">{formattedDate}</p>
+      <span onClick={() => deleteDocument(not.id)} className='material-symbols-outlined'>delete_forever</span>
     </div>
   );
 }
